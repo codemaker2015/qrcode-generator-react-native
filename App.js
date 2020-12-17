833dac33241708295e39c0a773302d735613fd01
Vishnu Sivan <codemaker2015@gmail.com>
2020-12-17 22:56:21 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import QRCode from 'react-native-qrcode-generator';
+ 
+import {
+    StyleSheet,
+    View,
+    TextInput
+} from 'react-native';
+ 
+class App extends Component {
+  state = {
+    text: 'https://github.com/codemaker2015',
+  };
+ 
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({text: text})}
+          value={this.state.text}
+        />
+        <QRCode
+          value={this.state.text}
+          size={200}
+          bgColor='black'
+          fgColor='white'/>
+      </View>
+    );
+  };
+}
+ 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+ 
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        borderRadius: 5,
+        padding: 5,
+    }
+});
+ 
+export default App;
